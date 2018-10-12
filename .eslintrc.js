8649c37feda439053760a0587de24321a05f3084
Azon <syu.takayama@gmail.com>
2018-10-12 11:41:03 +0900

    eslint --init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "standard",
+    "env": {
+        "mocha": true
+    },
+    "globals": {
+        "artifacts": true,
+        "contract": true,
+        "assert": true,
+        "web3": true
+    }
+};
